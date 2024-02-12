5cf05ec1d59395710b890e0e4b6143231adeaf05
jamy6564 <115299857+jamy6564@users.noreply.github.com>
2024-02-12 14:30:48 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,18 @@
-int main () {
-    DoublyLinkedList myList;
-    myList.insert(2, 0);
-    myList.insert(4, 1);
-    myList.insert(1, 0);
-    myList.insert(5, 3);
-    myList.insert(3, 2);
-    myList.print();             //1->2->3->4->5->
-    myList.printReverse();      //5->4->3->2->1->
+#include <iostream>
+#include <cmath>
+using namespace std;
 
-    myList.remove(0);
-    myList.remove(3);
-    myList.remove(1);
-    myList.print();             //2->4->
-    myList.printReverse();      //4->2->
-    
-    // search the target value
-    Node* target_ptr;
-    target_ptr = myList.search(100);
-    target_ptr = myList.search(1);
-
+int main()
+{
+    float TPR;
+    float FPR;
+    float Score;
+    cout << "Enter the True positive Rate: ";
+    cin >> TPR;
+    cout << "Enter the False Positive Rate: ";
+    cin >> FPR;
+    Score = 1 - sqrt(pow(1-TPR,2)+ pow(FPR,2));
+    cout << "The Score is: "<< Score;
     return 0;
+
 }
